fee30e3161b55c431371e158f55a42269ebac677
djwedge <djwedge3@hotmail.com>
2015-12-13 21:52:01 +0100

    Update libft.h

--- libft.h
+++ libft.h
@@ -21,12 +21,13 @@ void    *ft_memccpy(void *dest, const void *src, int c, size_t n);
 void    *ft_memmove(void *dest, const void *src, size_t n);
 void    *ft_memchr(const void *s, int c, size_t n);
 int     ft_memcmp(const void *s1, const void *s2, size_t n);
-int		ft_strlen(const char *s);
+size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *src);
 char	*ft_strcpy(char *dest, const char *src);
 char	*ft_strncpy(char *dest, const char *src, size_t n);
 char	*ft_strcat(char *dest, const char *src);
 char	*ft_strncat(char *dest, const char *src, size_t n);
+size_t  ft_strlcat(char *dest, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char    *ft_strstr(const char *str, const char *to_find);
@@ -47,6 +48,6 @@ void	ft_putendl(char const *s);
 void	ft_putnbr(int nbr);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char const *s, int fd);
-void	ft_putendl_fd(char const *s, int fd)
+void	ft_putendl_fd(char const *s, int fd);
 
 #endif
