c928caaa3cbc0355923d98e5e5e93dc190e7556b
djwedge <djwedge3@hotmail.com>
2015-12-16 20:55:06 +0100

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,7 +1,7 @@
 #include "libft.h"
 #include <string.h>
 
-size_t	strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	dst_len;
 	size_t	i;
